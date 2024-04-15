0980793823d396b53b8105fc4c61a9139a1282ad
Tony Enerson <tony@enerson.ca>
2024-04-15 10:36:12 -0600

    Add command line parameters

--- server/superheroes/createSuperhero.js
+++ server/superheroes/createSuperhero.js
@@ -1,9 +1,18 @@
 import { disconnectDatabase, createSuperhero } from "./superheroes.js";
 
+if (process.argv.length < 5) {
+    console.log('Usage: createSuperhero "name" "costume" "superpowers"')
+    process.exit(0)
+}
+
+const name = process.argv[2]
+const costume = process.argv[3]
+const superPowers = process.argv[4]
+
 console.log(await createSuperhero({
-    name: 'Superman',
-    costume: 'Blue body suit with red cape.',
-    superPowers: 'invincible, super strength, super speed, flight, laser beams'
+    name, 
+    costume, 
+    superPowers
 }))
 
 await disconnectDatabase();
